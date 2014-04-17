1011a8c414eef5a1440aa988c711c08c66de737b
Clarke Rahrig <crahrig@gmail.com>
2014-04-17 17:46:10 -0500

    Moved the MainActivity file into the new layout package.

---
+++ src/com/example/tweakbase/layout/MainActivity.java
@@ -0,0 +1,112 @@
+package com.example.tweakbase.layout;
+
+import com.example.tweakbase.R;
+import com.example.tweakbase.SettingsActivity;
+import com.example.tweakbase.adapters.TabsPagerAdapter;
+
+import android.annotation.TargetApi;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuItem;
+
+@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+public class MainActivity extends FragmentActivity implements
+		ActionBar.TabListener {
+
+	private ViewPager viewPager;
+	private TabsPagerAdapter mAdapter;
+	private ActionBar actionBar;
+	// Tab titles
+	private String[] tabs = { "Applications", "Ringermode"};
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity);
+
+		// Initialization
+		viewPager = (ViewPager) findViewById(R.id.pager);
+		actionBar = getActionBar();
+		mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+
+		viewPager.setAdapter(mAdapter);
+		actionBar.setHomeButtonEnabled(false);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);		
+
+		// Adding Tabs
+		for (String tab_name : tabs) {
+			actionBar.addTab(actionBar.newTab().setText(tab_name)
+					.setTabListener(this));
+		}
+
+		/**
+		 * on swiping the viewpager make respective tab selected
+		 * */
+		viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+			@Override
+			public void onPageSelected(int position) {
+				// on changing the page
+				// make respected tab selected
+				actionBar.setSelectedNavigationItem(position);
+			}
+
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+			}
+		});
+	}
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		viewPager.invalidate();
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// on tab selected
+		// show respected fragment view
+		viewPager.setCurrentItem(tab.getPosition());
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item){
+		// Handle item selection
+		switch (item.getItemId())
+		{
+		case R.id.action_settings:
+		// Launch the settings activity
+		Intent settings = new Intent(MainActivity.this, SettingsActivity.class);
+		startActivity(settings);
+		return true;
+		case R.id.view_data:
+		Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://whester.com/tweakbase/uploads/"));
+		startActivity(browserIntent);
+		default:
+		return super.onOptionsItemSelected(item);
+	}
+}
+
+}
